807707d0d56324004ef2e01815694b683955b88d
Faroeq <alves_1044@hotmail.com>
2024-08-30 19:04:33 +0200

    update welcome test description

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import app from '../src/app';
 
-describe('GET /api/v1', () => {
-	it('responds with a json message', (done) => {
+describe('GET /api/v1 welcome', () => {
+	it('responds with a welcome message', (done) => {
 		request(app)
 			.get('/api/v1/welcome')
 			.set('Accept', 'application/json')
