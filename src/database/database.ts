aea4a4e18f980d0d32f219b1b3785b3450bbaa6d
Faroeq <alves_1044@hotmail.com>
2024-09-02 22:23:49 +0200

    Fix duplicate database instance problem

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,38 +6,29 @@ dotenv.config()
 class Database {
 	private static _database: Database
 	private constructor() {
-		connectDB()
-		// mongoose.connect(dbUrl)
-		// 	.then(() => console.log('Connected with database'))
-		// 	.catch(() => console.log('Not connected with database'))
+		let uri = "";
+		if (process.env.NODE_ENV === "production") {
+			if (!process.env.MONGODB_CONNECTIONSTRING) {
+				throw new Error("MONGODB_CONNECTIONSTRING is not defined");
+			}
+			uri = process.env.MONGODB_CONNECTIONSTRING;
+
+		} else {
+			uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+			console.log("connectionstring: ", uri)
+		}
+
+		mongoose.connect(uri)
+			.then(() => console.log("Connected to MongoDB"))
+			.catch((err) => console.error("Could not connect to MongoDB...", err));
+
 	}
 	static getInstance() {
 		if (this._database) {
 			return this._database
 		}
 		this._database = new Database()
-		return this._database = new Database()
-	}
-}
-
-export const connectDB = async () => {
-	let uri = "";
-	if (process.env.NODE_ENV === "production") {
-		if (!process.env.MONGODB_CONNECTIONSTRING) {
-			throw new Error("MONGODB_CONNECTIONSTRING is not defined");
-		}
-		uri = process.env.MONGODB_CONNECTIONSTRING;
-		;
-	} else {
-		uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
-		console.log("connectionstring: ", uri)
-	}
-
-	try {
-		await mongoose.connect(uri);
-		console.log("Connected to MongoDB");
-	} catch (err) {
-		console.error("Could not connect to MongoDB...", err);
+		return this._database;
 	}
 }
 
